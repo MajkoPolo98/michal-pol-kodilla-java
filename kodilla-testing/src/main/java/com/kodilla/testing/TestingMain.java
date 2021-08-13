4430e46adbef7cfda462ae55fa3619a5e5079946
Michal Pol <dissected9@gmail.com>
2021-08-13 14:07:42 +0200

    6.2 Added kodilla.testing.calculator module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator paraLiczb = new Calculator(12, 9);
+        int add = paraLiczb.add();
+        int sub = paraLiczb.subtract();
+
+        System.out.println("Test jednostkowy - dodawanie:");
+        if(add == (paraLiczb.getA()+ paraLiczb.getB())){
+            System.out.println("Test OK");
+        }
+        System.out.println("Test jednostkowy - odejmowanie:");
+        if(sub == (paraLiczb.getA()- paraLiczb.getB())){
+            System.out.println("Test OK");
+        }
+
+
     }
 }
