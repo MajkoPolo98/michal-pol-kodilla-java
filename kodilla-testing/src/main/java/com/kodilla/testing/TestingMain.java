2909af86fbf1138575248b4c551a1a566d1105bb
Michal Pol <dissected9@gmail.com>
2021-08-15 22:39:13 +0200

    6.6 Added extended Book Library module (Mockito, JUnit)

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,32 +1,8 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
 
     public static void main(String[] args) {
-        /* W komentarzu, abym mógł wykonać dalsze zadania
-        Calculator paraLiczb = new Calculator();
-        int add = paraLiczb.add(12, 9);
-        int sub = paraLiczb.subtract(15 ,7);
-
-        System.out.println("Test jednostkowy - dodawanie:");
-        if(add == 21){
-            System.out.println("Test OK");
-        } else {
-            System.out.println("Error");
-        }
-        System.out.println("Test jednostkowy - odejmowanie:");
-        if(sub == 8){
-            System.out.println("Test OK");
-        } else {
-            System.out.println("Error");
-        }
-
-
-    }
-         */
 
     }
 }
