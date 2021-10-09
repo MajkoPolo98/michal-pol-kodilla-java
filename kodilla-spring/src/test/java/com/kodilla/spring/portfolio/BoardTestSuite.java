4df5f13f5be8f50556dca7fd8499434bf4ba021a
Michal Pol <dissected9@gmail.com>
2021-10-09 23:10:48 +0200

    14.3 Qualifier

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BoardTestSuite {
+
+    @Test
+    void testTaskAdd(){
+
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+
+        //When
+        board.getToDoList().getTasks().add("TASK TO DO");
+        board.getInProgressList().getTasks().add("TASK IN PROGRESS");
+        board.getDoneList().getTasks().add("DONE TASK");
+
+        //Then
+        System.out.println(board.getToDoList().getTasks().get(0));
+        System.out.println(board.getInProgressList().getTasks().get(0));
+        System.out.println(board.getDoneList().getTasks().get(0));
+    }
+}
